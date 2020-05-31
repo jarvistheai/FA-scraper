8e9511f92699006e679072ada2a489cde80b7429
Jarvis Moreau <nico.genti96@gmail.com>
2020-05-31 10:53:33 +0200

    update files

--- test.py
+++ test.py
@@ -8,16 +8,7 @@ for line in f:
     key, value = line.split()
     cookies[key] = value
 
-print(cookies)
+r = requests.get('https://www.furaffinity.net/user/j.a.r.v.i.s.', cookies=cookies)
 
-test = []
-for line in f:
-    test.append(line.split())
-
-print(test)
-
-
-# r = requests.get('https://www.furaffinity.net/user/j.a.r.v.i.s.', cookies=cookies)
-#
-# with open('file.html', 'w') as f:
-#     f.write(r.text)
+with open('file.html', 'w') as f:
+    f.write(r.text)
